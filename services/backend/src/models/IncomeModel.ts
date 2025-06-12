738c9ddcbfbb42c0a9f820d79bf63a8626bd0e6d
Ramadan Velisha <ramadan.velisha@artwarebg.com>
2025-06-13 00:21:16 +0300

    Create IncomeModel.ts

---
+++ services/backend/src/models/IncomeModel.ts
@@ -0,0 +1,50 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IIncome extends Document {
+  title: string;
+  amount: number;
+  type: string;
+  date: Date;
+  category: string;
+  description: string;
+}
+
+const IncomeSchema: Schema<IIncome> = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      max: 1_000_000, 
+      trim: true,
+    },
+    type: {
+      type: String,
+      default: 'income',
+    },
+    date: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 20,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IIncome>('Income', IncomeSchema);
