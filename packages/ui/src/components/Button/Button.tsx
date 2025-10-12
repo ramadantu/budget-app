2123fe4473e23e741eee85df4abc66d64f75c930
Ramadan Velisha <ramadan.velisha@gmail.com>
2025-10-13 02:14:55 +0300

    chore: trigger release

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -18,6 +18,7 @@ function Button({ name, icon, onClick, bg, bPad, color, bRad }: ButtonProps) {
         padding: bPad,
         borderRadius: bRad,
         color: color,
+        width: name ? 'auto' : 'fit-content',
       }}
       onClick={onClick}
     >
