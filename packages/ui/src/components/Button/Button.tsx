92fb0143438e928efe3fd9fd579013ff4963e8c6
Ramadan Velisha <ramadan.velisha@gmail.com>
2025-10-12 22:52:49 +0300

    chore: test Web release on UI package change (#7)

--- packages/ui/src/components/Button/Button.tsx
+++ packages/ui/src/components/Button/Button.tsx
@@ -14,7 +14,7 @@ function Button({ name, icon, onClick, bg, bPad, color, bRad }: ButtonProps) {
   return (
     <ButtonStyled
       style={{
-        background: bg,
+        background: 'red',
         padding: bPad,
         borderRadius: bRad,
         color: color,
