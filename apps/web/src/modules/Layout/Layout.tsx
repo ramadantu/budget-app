91c1d60ad7be602c29fa334255b2e068596bc85f
Ramadan Velisha <ramadan.velisha@gmail.com>
2025-11-30 01:30:31 +0200

    Update Layout.tsx

--- apps/web/src/modules/Layout/Layout.tsx
+++ apps/web/src/modules/Layout/Layout.tsx
@@ -1,12 +1,7 @@
-import { useMemo } from 'react'
-import { SessionProvider, useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import styled from 'styled-components'
 
 import Nav from '../Nav'
-import { GlobalProvider } from '../../context/globalContext'
-import { GlobalStyle } from '../../styles/GlobalStyle'
-
-import Orb from './Orb'
 
 const StyledLayout = styled.div`
   padding: 2rem;
@@ -15,48 +10,12 @@ const StyledLayout = styled.div`
   gap: 2rem;
 `
 
-const AppStyled = styled.div`
-  height: 100vh;
-  background-image: url('./img/bg.png');
-  position: relative;
-  main {
-    flex: 1;
-    background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #ffffff;
-    backdrop-filter: blur(4.5px);
-    border-radius: 32px;
-    overflow-x: hidden;
-    &::-webkit-scrollbar {
-      width: 0;
-    }
-  }
-`
-
-function Layout({ children }: { children?: React.ReactNode }) {
+export default function Layout({ children }: { children?: React.ReactNode }) {
   const { data } = useSession({ required: true })
-
-  const OrbMemoized = useMemo(() => {
-    return <Orb />
-  }, [])
-
   return (
-    <GlobalProvider>
-      <GlobalStyle />
-      <AppStyled>
-        {OrbMemoized}
-        <StyledLayout>
-          <Nav username={data?.user?.name} />
-          <main>{children}</main>
-        </StyledLayout>
-      </AppStyled>
-    </GlobalProvider>
-  )
-}
-
-export default function AuthLayout({ children }: { children?: React.ReactNode }) {
-  return (
-    <SessionProvider>
-      <Layout>{children}</Layout>
-    </SessionProvider>
+    <StyledLayout>
+      <Nav username={data?.user?.name} />
+      <main>{children}</main>
+    </StyledLayout>
   )
 }
