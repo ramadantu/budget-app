b0a82b07b7dcf66cf418c3610fab9465d43d5055
Ramadan Velisha <ramadan.velisha@gmail.com>
2026-01-30 04:46:54 +0200

    update

--- services/server/src/index.ts
+++ services/server/src/index.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+// import Keycloak from 'keycloak-connect'
 
+// import config from './keycloak'
 import transactionRoutes from './routes/transactions'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
+// const keycloak = new Keycloak({}, config)
 
 app.use(cors())
 app.use(express.json())
+// app.use(keycloak.middleware())
+// app.use(keycloak.protect())
 app.use('/api/v1/', transactionRoutes)
 
 const server = () => {
